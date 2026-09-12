41461e0a3a67ee2cc7698f7d6a97bfcf1fddec11
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-12 15:36:59 -0400

    Update Day.py

--- Interval/Day.py
+++ Interval/Day.py
@@ -1,17 +1,8 @@
-from . import IS_SERVER, main_repo
+from philh_myftp_biz.modules.Module import Module
+from . import IS_SERVER
 
 if IS_SERVER:
 
-    main_repo.reset()
+    MC = Module('E:/Minecraft/')
+    MC.run('backup')
 
-    main_repo.add('/Minecraft/Worlds/')
-    main_repo.add('/Plex/Plex Media Server/Plug-in Support/Databases/')
-
-    if main_repo.changes > 0:
-
-        new_commit = main_repo.commit(
-            message = f"Automated Backup",
-            skip_hooks = True,
-        )
-
-        main_repo.push()
