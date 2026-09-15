296b0aa49541a49f9ce9b4d6e7794e8cf88bb456
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-15 05:01:02 -0400

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 
 try:
-    import philh_myftp_biz, pybind11_stubgen, wmi # pyright: ignore[reportUnusedImport]
+    import philh_myftp_biz, wmi # pyright: ignore[reportUnusedImport]
 
 except ModuleNotFoundError:
     from subprocess import run
@@ -8,7 +8,6 @@ except ModuleNotFoundError:
 
     pip('install', '-U', 'git+https://github.com/MineFartS/Server-PythonPackage.git')
 
-    pip('install', 'pybind11-stubgen')
     pip('install', 'wmi')
 
     run(
