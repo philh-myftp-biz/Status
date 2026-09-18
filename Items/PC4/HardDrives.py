13c960ea0cb4e9443376337986339cd61fb76a03
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-18 08:32:45 -0400

    Add PC-4 Items

---
+++ Items/PC4/HardDrives.py
@@ -0,0 +1,13 @@
+from philh_myftp_biz.pc.hardware import HardDrive
+
+Items: list[HardDrive] = [
+
+    HardDrive(
+        Tower = 'A',
+        Conn = 'SATA',
+        ID = 1,
+        SN = 'WD-WX21A745H514'
+    ),
+
+]
+
