5d0c01efd7b3b7c0e2decab135c382158ede7023
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-23 05:12:29 -0400

    Update Api.py

--- run/Api.py
+++ run/Api.py
@@ -1,20 +1,23 @@
 from philh_myftp_biz.pc.hardware import Device
 from philh_myftp_biz.terminal import Args
+from collections import defaultdict
 from json import dumps
 from .. import Items
 
-data = {}
-
 items: list[Device] = getattr(Items, Args[0])
 
+cls = (items[0].__class__ if items else object).__dict__ | Device.__dict__
+
+props = list(filter(
+    lambda k: not (k.startswith('_') or callable(cls[k])),
+    cls.keys()
+))
+
+data = defaultdict(dict)
+
 for item in items:
-
-    data[item.Name] = {}
-
-    for var, val in vars(item).items():
-
-        if var[0] != '_':
-            data[item.Name][var] = val
+    for prop in props:
+        data[item.Name][prop] = getattr(item, prop)
 
 print(dumps(
     obj = data,
